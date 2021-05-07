477a3e7b6fcaf21b85162679c0386b6ab2facdbf
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-05-07 15:53:36 +0530

    Included ping router ✔

--- src/tools/router.js
+++ src/tools/router.js
@@ -1,8 +1,9 @@
 import { errorHandler } from 'middlewares';
-import { AdminRouter } from 'routers';
+import { AdminRouter, PingRouter } from 'routers';
 
 export default function registerRouters(app) {
 	app.use('/admin', AdminRouter);
+	app.use('/ping', PingRouter);
 
 	app.use(errorHandler);
 }

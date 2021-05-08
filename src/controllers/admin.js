70c1e4441203f708e7ea515a784929004fe74c97
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-05-08 18:05:34 +0530

    Added admin controllers ✌

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -29,6 +29,71 @@ export const newStudent = async (req, res) => {
 
 /**
  *
+ * Get list of students
+ *
+ * @route: /getStudents
+ * @method: GET
+ * @requires: body{}
+ * @returns: Object{Students}
+ *
+ */
+
+export const getStudents = async (req, res) => {
+	const students = await Student.find();
+	return res.status(200).send(students);
+};
+
+/**
+ *
+ * Get a student by id
+ *
+ * @route: /getStudent
+ * @method: GET
+ * @requires: body{id}
+ * @returns: Object{Student}
+ *
+ */
+export const getStudentById = async (req, res) => {
+	const { id } = req.params;
+	logger.info(id);
+
+	if (!id) return res.status(400).send('id field required');
+
+	if (!Mongoose.Types.ObjectId.isValid(id)) return res.status(400).send('id must be valid');
+
+	const student = await Student.findById(id);
+
+	if (!student) return res.status(404).send('Student does not exist');
+
+	return res.status(200).send(student);
+};
+
+/**
+ *
+ * Get a student by register number
+ *
+ * @route: /getStudent
+ * @method: GET
+ * @requires: body{registerNumber}
+ * @returns: Object{Student}
+ *
+ */
+export const getStudentByRegisterNumber = async (req, res) => {
+	const { registerNumber } = req.params;
+
+	if (!registerNumber) return res.status(400).send('registerNumber field required');
+
+	if (!Number(registerNumber)) return res.status(400).send('registerNumber must be valid');
+
+	const student = await Student.find({ registerNumber });
+
+	if (!student) return res.status(404).send('Student does not exist');
+
+	return res.status(200).send(student);
+};
+
+/**
+ *
  * Delete Student
  *
  * @route: /student/delete
@@ -103,4 +168,20 @@ export const deleteStaff = async (req, res) => {
 	if (!staff) return res.status(404).send('Staff does not exist');
 	logger.debug('Staff deleted successfully');
 	return res.status(200).send('Staff deleted successfully');
+};
+
+/**
+ *
+ * Get list of staffs
+ *
+ * @route: /getStaffs
+ * @method: GET
+ * @requires: body{}
+ * @returns: Object{Staffs}
+ *
+ */
+
+export const getStaffs = async (req, res) => {
+	const staffs = await Staff.find();
+	return res.status(200).send(staffs);
 };
--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,13 +1,26 @@
 import { Router } from 'express';
 
-import { deleteStaff, deleteStudent, newStaff, newStudent } from 'controllers/admin';
-import { errorHandler } from 'middlewares';
+import {
+	deleteStaff,
+	deleteStudent,
+	getStudents,
+	newStaff,
+	newStudent,
+	getStaffs,
+	getStudentById,
+	getStudentByRegisterNumber
+} from 'controllers/admin';
 
 const router = Router();
 
-router.post('/student/new', errorHandler, newStudent);
-router.delete('/student/delete', errorHandler, deleteStudent);
-router.post('/staff/new', errorHandler, newStaff);
-router.delete('/staff/delete', errorHandler, deleteStaff);
+router.post('/student/new', newStudent);
+router.delete('/student/delete', deleteStudent);
+router.get('/student/all', getStudents);
+router.get('/student/:id', getStudentById);
+router.get('/student/:registerNumber', getStudentByRegisterNumber);
+
+router.post('/staff/new', newStaff);
+router.delete('/staff/delete', deleteStaff);
+router.get('/staff/all', getStaffs);
 
 export default router;

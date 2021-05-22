d17b7089fcf9f02a2972f75453b0ee0821e668c4
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2021-05-22 11:23:38 +0530

    Minor fixes (#7)
    * improvements in using config variables
    1. removed dotenv config duplicates
    2. switched from process env PORT to config port
    3. changed log port value from absolute to runtime assigned value
    
    * fixed jsconfig type problem

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
-require('dotenv').config();
 import express from 'express';
 import chalk from 'chalk';
 import 'express-async-errors';
 import cors from 'cors';
 
-// Must import dotenv config before config
 import 'dotenv/config';
+import config from 'config';
 
 import { registerLogging, registerPreprocessor, registerRouters } from 'tools';
 import { dbConnection } from './dbConnection';
 import logger from 'tools/logging';
 
-const PORT = process.env.PORT;
+const PORT = config.get('port');
 
 const app = express();
 app.use(cors());
@@ -23,7 +22,8 @@ registerRouters(app);
 const server = app.listen(PORT);
 
 server.once('listening', async () => {
-	logger.info(`Server started at port ${chalk.magenta(PORT)}`);
+	const { port } = server.address();
+	logger.info(`Server started at port ${chalk.magenta(port)}`);
 	await dbConnection();
 });
 
